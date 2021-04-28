aed7060b764ee331fe6ae004d433c90d3a4c71bf
Marwennnne <marwen.guesmi@insat.u-carthage.tn>
2021-04-28 02:25:48 +0100

    env

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
+require('dotenv').config();
 const app = require("express")();
 const mongoose = require('mongoose');
 const RouterInterface = require('./routers/interface');
 const bodyParser = require('body-parser');
 const interface = require('./models/Interface')
-mongoose.connect('mongodb://localhost:27017/testMirror', {useNewUrlParser: true})
-.then(() => {
-    console.log("mongoose connected")
-})
+
+try{
+    mongoose.connect(`mongodb+srv://${process.env.user}:${process.env.password}@cluster0.yf7o0.mongodb.net/testMirror?retryWrites=true&w=majority`, {useNewUrlParser: true})
+    .then(() => {
+        console.log("mongoose connected")
+    });
+} catch(err){
+    console.log(err.message);
+}
+
 
 app.get('/',(req,res) => {
     res.status(200).json({ message: "hello world"});
 });
+
 
 app.use(bodyParser.json());
 app.use(RouterInterface);
